24c9a3e424884bc1111f89fdf3fc609f461b64c5
Keith <keithkng73@gmail.com>
2017-03-09 21:15:54 -0700

    Modified files to be deployment agnostic, also added file to install wordpress server

--- setupTicket.sh
+++ setupTicket.sh
@@ -14,19 +14,11 @@ sudo ln -fs /usr/share/phpmyadmin /var/www/phpmyadmin
 sudo sed -i '23s:.*:        php_admin_value open_basedir "none":' /etc/phpmyadmin/apache.conf
 sudo service apache2 restart
 
-mysql -u root -pP@ssw0rd << EOF
-CREATE USER 'osticketuser'@'localhost' IDENTIFIED WITH mysql_native_password AS 'P@ssw0rd';
-GRANT ALL PRIVILEGES ON *.* TO 'osticketuser'@'localhost' REQUIRE NONE WITH GRANT OPTION MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
-CREATE DATABASE IF NOT EXISTS `osticketuser`;
-GRANT ALL PRIVILEGES ON `osticketuser`.* TO 'osticketuser'@'localhost';
-GRANT ALL PRIVILEGES ON `osticketuser\_%`.* TO 'osticketuser'@'localhost';
-EOF
-
 cd /tmp
 sudo wget http://osticket.com/sites/default/files/download/osTicket-v1.10.zip
 sudo unzip osTicket* -d ticket
 sudo cp -r ticket/upload/* /var/www/html
-sudo chown -R administrator:www-data /var/www/html
+sudo chown -R `whoami`:www-data /var/www/html
 
 cd /var/www/html
 cp include/ost-sampleconfig.php include/ost-config.php
@@ -35,3 +27,10 @@ chmod -R g+s /var/www/html
 find /var/www/html -type d -exec chmod 0755 {} \;
 find /var/www/html -type f -exec chmod 0644 {} \;
 chmod 0666 include/ost-config.php
+
+echo '!!!'
+echo '!!!'
+echo "Make sure to create a user and a database for that user in PHPmyAdmin before running the osTicket installer!"
+echo '!!!'
+echo '!!!'
+read -p "Press enter to continue..."
---
+++ setupWordpress.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd ~
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y install lamp-server^
+sudo apt -y install phpmyadmin
+
+cd /tmp
+sudo wget https://wordpress.org/latest.tar.gz
+sudo tar -xvf latest.tar.gz wordpress
+sudo cp -r wordpress/* /var/www/html
+
+cd /var/www/html
+sudo chown -R `whoami`:www-data /var/www/html
+chmod -R g+s /var/www/html
+find /var/www/html -type d -exec chmod 0755 {} \;
+find /var/www/html -type f -exec chmod 0644 {} \;
+
+sudo service apache2 restart
+
+echo '!!!'
+echo '!!!'
+echo "Make sure to create a user and a database for that user in PHPmyAdmin before running the wordpress installer!"
+echo '!!!'
+echo '!!!'
+read -p "Press ENTER to continue..."
