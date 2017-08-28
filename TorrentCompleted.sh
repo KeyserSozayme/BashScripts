c9b75cf9ec74864d8dc5e9c2df72991fd4860d95
Keith <KingKeithC@gmail.com>
2017-08-28 17:24:54 -0600

    Removed the need for auth Added comments

--- TorrentCompleted.sh
+++ TorrentCompleted.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/env bash
 
-export TR_AUTH=:
 TOR_FILE="$TR_TORRENT_DIR/$TR_TORRENT_NAME"
 NOW="$(date +"%F %T")"
 EMAIL="/tmp/Email $NOW.log"
-RPC="transmission-remote localhost:11920 -ne"
+RPC="transmission-remote localhost:11920"
 
 # Remove Torrent
 ${RPC} -t "$TR_TORRENT_ID" --remove
 
+# Email Notification to be Sent
 cat > "$EMAIL" << EOF
 At $TR_TIME_LOCALTIME the following file finished downloading...
 
@@ -17,6 +17,7 @@ At $TR_TIME_LOCALTIME the following file finished downloading...
 Thank You
 EOF
 
+# Send Email
 cat "$EMAIL" | mutt -s 'A torrent Has Completed!' KingKeithC@gmail.com
 rm "$EMAIL" "$HOME/sent"
 
